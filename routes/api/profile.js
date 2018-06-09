457083e543cca94c53220cbe0ea3ded2d9f6d91d
azmul <azmulg@gmail.com>
2018-06-09 09:08:26 +0600

    add profile route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -32,4 +32,57 @@ router.get('/', passport.authenticate('jwt',{session: false}),
                }).catch(err=> res.status(404).json(err));
     }
 )
+
+// @route  POST /api/profile
+// @desc   POST  user profile
+// @route  private
+router.post('/', passport.authenticate('jwt',{session: false}),
+    (req,res)=>{
+        const profileFileds = {},errors = {};
+        profileFileds.user = req.user.id;
+
+        if(req.body.handle) profileFileds.handle = req.body.handle;
+        if(req.body.company) profileFileds.company = req.body.company;
+        if(req.body.website) profileFileds.website = req.body.website;
+        if(req.body.location) profileFileds.location = req.body.location;
+        if(req.body.bio) profileFileds.bio = req.body.bio;
+        if(req.body.githubusername) profileFileds.githubusername = req.body.githubusername;
+        // Skills splits into array
+        if(typeof req.body.skills !== undefined){
+          profileFileds.skills = req.body.skills.split(',');
+        }
+        // Socials
+        profileFileds.social = {};
+        if(req.body.youtube) profileFileds.social.youtube = req.body.youtube;
+        if(req.body.twitter) profileFileds.social.twitter = req.body.twitter;
+        if(req.body.facebook) profileFileds.social.facebook = req.body.facebook;
+        if(req.body.linkedin) profileFileds.linkedin = req.body.linkedin;
+        if(req.body.instagram) profileFileds.instagram = req.body.instagram;
+
+        Profile.findOne({user: req.user.id})
+               .then(profile=>{
+                   if(profile){
+                       // Update
+                       Profile.findOneAndUpdate(
+                           {user: req.user.id},
+                           {$set: profileFileds}, 
+                           {new: true})
+                           .then(profile=>res.json(profile));
+                   }else{
+                       // Check if handle exits
+                       Profile.findOne({handle: profileFileds.handle})
+                            .then(profile=>{
+                                if(profile){
+                                    errors.handle = 'That handle already exits';
+                                    res.status(400).json(errors);
+                                }
+                                new Profile(profileFileds).save().then(profile=>res.json(profile));
+                            })
+                   }
+               })
+        
+    }
+)
+
+
 module.exports = router;
